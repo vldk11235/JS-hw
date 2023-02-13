413e2f33b98be5ccf21be38d1d8b35f696e0f72d
Vlad Kremenchuk <vlad.kremenchuk@gmail.com>
2023-02-13 22:43:09 +0200

    first js hw done

---
+++ jsHw1/script.js
@@ -0,0 +1,23 @@
+let greetings = 'Hello, ' + prompt('What is your name?');
+//создали переменную, которая принимает в себя ответ пользователя и добавляет Hello,
+console.log(greetings);
+//выводим итог в консоль лог
+
+/*
+Здесь щас будем считать возраст пользователя
+будем отнимать текущий год от даты рождения
+результат выведу в консоль лог
+ */
+
+const currentYear = 2023;
+let userAge = currentYear - prompt('Enter your birth year');
+console.log(userAge);
+
+//Работает!!!
+
+//тут щас будем считать периметр квадрата
+
+let perimeter = prompt('What a length of a square side is? ') * 4;
+console.log(perimeter);
+
+//периметр квадрата посчитан
