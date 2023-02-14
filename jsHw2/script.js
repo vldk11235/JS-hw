438cf82e0c33d2eed6c08af9f22ffd71937ceb38
Vlad Kremenchuk <vlad.kremenchuk@gmail.com>
2023-02-14 21:50:52 +0200

    all done

--- jsHw2/script.js
+++ jsHw2/script.js
@@ -1,12 +1,23 @@
 let moneyAvailable = prompt('How much money do you have?');
 let chocolatePrice = prompt('How much does the chocolate cost?');
 
-let chocolateToBuy = moneyAvailable / chocolatePrice;
+let chocolateToBuy = Math.floor(moneyAvailable / chocolatePrice);
+
 let moneyLeft = moneyAvailable - (chocolatePrice * chocolateToBuy);
 
+alert(`You can buy ${chocolateToBuy} chocolates`);
+alert(`You will have ${moneyLeft} money left`);
 
-// console.log(chocolateToBuy);
-// console.log(moneyLeft);
-alert('You can buy' + ' ' + chocolateToBuy + ' ' + 'chocolates');
-alert('You will have' + ' ' + moneyLeft + ' ' + 'money left');
+let userNumber = +prompt('Type a 3 digit number');
+
+let firstDigit = userNumber % 10;
+let secondDigit = (userNumber % 100 - firstDigit) / 10;
+let thirdDigit = Math.floor((userNumber % 1000 - (firstDigit + secondDigit)) / 100);
+
+
+alert(`Your number backwards is ${firstDigit}${secondDigit}${thirdDigit}`);
+
+
+
+
 
