0bd696099547b903cf2759f0aac1ddca67028d5b
Vlad Kremenchuk <vlad.kremenchuk@gmail.com>
2023-02-18 15:02:22 +0200

    next day hw fixed

--- jsHw3/script.js
+++ jsHw3/script.js
@@ -55,10 +55,10 @@ while (Boolean(numberIsRight === false)) {
 //
 //
 // }
-
-
+//
+//
 // let numberIsMore = confirm(`Is your number over ${currentValue}?`);
-
+//
 
 let a = 2;
 for (a = 2; a <= 9; a++) {
@@ -70,47 +70,97 @@ for (a = 2; a <= 9; a++) {
 //it's easy whe we make cyncle inside the other cycle: we mace a counter for incrementing numbers,
 //and then make another one for multiplying each number 10 times in a row
 
+
+//
+// //asked the user for data
+//
+//
+// if (userDay <= 28 && userDay > 0) {
+//
+//     alert(`The next day is ${++userDay}.${userMonth}.${userYear}`);
+// }
+//
+// //the most common cases, when it's not the end of the month or a year
+//
+// if (userDay === 31 && userMonth === 12) {
+//     alert(`The next day is 01.01.${++userYear}`);
+// }
+// //the new year case Jingle Bells!!!
+//
+// if (userDay === 28 && userMonth === 2 && userYear % 4) {
+//     alert(`The next day is 29.02.${userYear}`);
+// }
+// // leap year case
+//
+// if (userDay === 29 && userMonth === 2 && userYear %4) {
+//     alert(`The next day is 01.03.${userYear}`)
+// }
+//
+// // leap year case 2
+//
+// if (userDay === 30 && (userMonth === 2 || 4 || 6 || 9 || 11 )) {
+//     alert(`The next day is 01.${++userMonth}.${userYear}`)
+// }
+//
+// //the las day of the 30-days months
+//
+// if (userDay === 31 && (userMonth === 1 || 3 || 5 || 7 || 8 || 10 ) ) {
+//     alert(`The next day is 01.${++userMonth}.${userYear}`)
+// }
 let userDay = +prompt('Enter the day');
 let userMonth = +prompt('Enter the month');
 let userYear = +prompt('Enter the Year');
 
-//asked the user for data
+let day = new Date(userYear, --userMonth, userDay);
+console.log(day); // Apr 30 2000
+
+let nextDay = new Date(day);
+nextDay.setDate(day.getDate() + 1);
+console.log(nextDay); // May 01 2000
+
+// //the last day of the 31-days months
+//
+// //to tell the truth i suppose that the last task need more sophisticated solution using cycles or something
+// //I just hardcoded that and it's not right i guess. I'll think tomorrow about that
 
 
-if (userDay <= 28 && userDay > 0) {
-
-    alert(`The next day is ${++userDay}.${userMonth}.${userYear}`);
-}
-
-//the most common cases, when it's not the end of the month or a year
-
-if (userDay === 31 && userMonth === 12) {
-    alert(`The next day is 01.01.${++userYear}`);
-}
-//the new year case Jingle Bells!!!
-
-if (userDay === 28 && userMonth === 2 && userYear % 4) {
-    alert(`The next day is 29.02.${userYear}`);
-}
-// leap year case
-
-if (userDay === 29 && userMonth === 2 && userYear %4) {
-    alert(`The next day is 01.03.${userYear}`)
-}
-
-// leap year case 2
-
-if (userDay === 30 && (userMonth === 2 || 4 || 6 || 9 || 11 )) {
-    alert(`The next day is 01.${++userMonth}.${userYear}`)
-}
-
-//the las day of the 30-days months
-
-if (userDay === 31 && (userMonth === 1 || 3 || 5 || 7 || 8 || 10 ) ) {
-    alert(`The next day is 01.${++userMonth}.${userYear}`)
-}
-
-//the last day of the 31-days months
-
-//to tell the truth i suppose that the last task need more sophisticated solution using cycles or something
-//I just hardcoded that and it's not right i guess. I'll think tomorrow about that
+// let userDay = +prompt('Enter the day');
+// let userMonth = +prompt('Enter the month');
+// let userYear = +prompt('Enter the Year');
+//
+// let data = new Date(userYear, userMonth, userDay);
+// let tomorrow = new Date();
+// tomorrow.setDate(data.getDate()+1);
+// console.log(tomorrow);
+// let userRadius;
+//
+// getData();
+// alert(circleLength());
+// alert(circleSquare());
+//
+//
+// function getData() {
+//     userRadius = +prompt('enter radius');
+// }
+//
+// function circleLength() {
+//
+//     if (userRadius > 0) {
+//         return (2 * (Math.PI * userRadius)).toFixed(2);
+//     } else {
+//         alert('enter valid value');
+//         getData();
+//         return circleLength();
+//     }
+// }
+//
+// function circleSquare() {
+//
+//     if (userRadius > 0) {
+//         return (Math.PI * userRadius ** 2).toFixed(2);
+//     } else {
+//         alert('enter valid value');
+//         getData();
+//         return circleSquare();
+//     }
+// }
